005b0d8f8d0eceee9b1445a5569b66222bc84046
karaage-2 <67582392+karaage-2@users.noreply.github.com>
2024-05-21 21:04:42 +0900

    Create UserScriptLoader.js

---
+++ UserScriptLoader.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.src = `https://cdn.rawgit.com/karaage-2/CrosshairMod/main/CrosshairMod.js`;
+script.type = 'text/javascript';
+document.body.appendChild(script);
