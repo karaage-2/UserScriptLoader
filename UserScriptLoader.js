ea801cd144dc49fec7076d542d3d8000ef4872c6
karaage-2 <67582392+karaage-2@users.noreply.github.com>
2024-05-22 08:38:26 +0900

    Update UserScriptLoader.js

--- UserScriptLoader.js
+++ UserScriptLoader.js
@@ -1,10 +1,10 @@
-if (document.domain = "bloxd.io" || document.domain = "kirka.io") {
+if (document.domain == "bloxd.io" || document.domain == "kirka.io") {
     let script1 = document.createElement('script');
     script1.src = `https://karaage-2.github.io/CrosshairMod/CrosshairMod.js`;
     script1.type = 'text/javascript';
     document.body.appendChild(script1);
 }
-if (document.domain = "kirka.io") {
+if (document.domain == "kirka.io") {
     const cssLink = "https://karaage-2.github.io/KirkaCSS/NizzQCustom.css";
     document.addEventListener("DOMContentLoaded", () => {
 
