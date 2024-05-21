cd14d320e796ddfdb689db36e7a035fad37f17d5
karaage-2 <67582392+karaage-2@users.noreply.github.com>
2024-05-21 23:04:21 +0900

    Update UserScriptLoader.js

--- UserScriptLoader.js
+++ UserScriptLoader.js
@@ -1,11 +1,11 @@
 if (document.domain = "bloxd.io" || document.domain = "kirka.io") {
     let script1 = document.createElement('script');
-    script1.src = `https://cdn.rawgit.com/karaage-2/CrosshairMod/main/CrosshairMod.js`;
+    script1.src = `https://karaage-2.github.io/CrosshairMod/CrosshairMod.js`;
     script1.type = 'text/javascript';
     document.body.appendChild(script1);
 }
 if (document.domain = "kirka.io") {
-    const cssLink = "https://cdn.rawgit.com/karaage-2/KirkaCSS/main/NizzQCustom.css";
+    const cssLink = "https://karaage-2.github.io/KirkaCSS/NizzQCustom.css";
     document.addEventListener("DOMContentLoaded", () => {
 
         let cssLinkElem = document.createElement("link");
